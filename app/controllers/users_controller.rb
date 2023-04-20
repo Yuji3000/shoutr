352f487886e87e67ac5bfaf9333b33885ba00bec
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-04-20 13:47:05 -0600

    Sign in with username or email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < Clearance::UsersController
   private
 
-  # def create
-  #   @user = User.new(users_params)
-  #   if @user.save
-  #       redirect_to '/'
-  #   end
-  # end
+  def create
+    @user = User.new(users_params)
+    if @user.save
+        redirect_to '/'
+    end
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
