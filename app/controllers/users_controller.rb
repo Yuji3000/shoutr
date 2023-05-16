cd7e3813d561ddccede7151468ba24552207d9bd
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-05-16 14:30:55 -0600

    Add follow and unfollow feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < Clearance::UsersController
   end
   
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(username: params[:id])
     @shouts = @user.shouts
   end
   
--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,9 +1,9 @@
 module ShoutHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest user.email
-    gravatar_url = "//www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}"
-    image_tag gravatar_url
-  end
+  # def avatar(user)
+  #   email_digest = Digest::MD5.hexdigest user.email
+  #   gravatar_url = "//www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}"
+  #   image_tag gravatar_url
+  # end
 
   def like_button(shout)
     if current_user.liked?(shout)
@@ -11,5 +11,9 @@ module ShoutHelper
     else
       link_to "Like", like_shout_path(shout), method: :post 
     end
+  end
+
+  def autolink(text)
+    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1])}.html_safe
   end
 end
